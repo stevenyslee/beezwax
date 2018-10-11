6286ab5195067b253dff326a82fe2a6cb3b73619
Steven Lee <stevenyslee@gmail.com>
2018-10-10 20:37:11 -0700

    Add POST endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,17 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  let data = {
+    message: 'Hello World!'
+  };
+  res.status(200);
+  res.send('Hello World!')
+});
+
+app.post('/', (req, res) => {
+  res.status(200)
+  res.send('Post Received!')
+});
 
 app.listen(3000, () => console.log('App listening on port 3000'));
