f5bd3354035f850a1c892dc429b14dfb714ee575
Steven Lee <stevenyslee@gmail.com>
2018-10-10 20:37:49 -0700

    Handle clicks and GET requests

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Form from './components/form.jsx';
+import $ from 'jquery';
  
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log(event);
+  }
+
+  componentDidMount() {
+    console.log('Component did mount');
+    let get = $.get( "http://localhost:3000/", (data) => {
+      console.log("Success!");
+      console.log(data);
+    }).fail(() => {
+        console.log("Error!");
+      });
+  }
+
   render() {
-    return (<div>Hello World</div>);
+    return (
+        <Form handleSubmit={this.handleSubmit} />
+      );
   }
 }
  
