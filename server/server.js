556d0af1c4be10b0fa47bd2d287b0e68a1b19f9f
Steven Lee <stevenyslee@gmail.com>
2018-10-10 20:45:58 -0700

    Get request body

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -12,6 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
+  console.log(req.body);
   res.status(200)
   res.send('Post Received!')
 });
